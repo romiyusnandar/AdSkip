f92724ac13ddb24b9eda148ecf660aaff0a0ea66
romiyusnandar <yusromi04@gmail.com>
2026-04-01 15:03:49 +0700

    Bump version to 2.4
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 2
-        versionName = "2.3"
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
