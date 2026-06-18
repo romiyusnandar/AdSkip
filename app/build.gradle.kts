11f6129a0dfa5c5b140af400c05dd472d93b92f3
romiyusnandar <yusromi04@gmail.com>
2026-06-18 16:53:58 +0700

    Bump to 2.6 version
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 2
-        versionName = "2.5"
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
